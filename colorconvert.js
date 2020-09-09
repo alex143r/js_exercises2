a2dd08a95ddc622f181133a02047ec05f617214f
Alexander <alex143r@stud.kea.dk>
2020-09-09 06:52:51 +0200

    exer2 colorconvert done

--- colorconvert.js
+++ colorconvert.js
@@ -1,8 +1,8 @@
 "use strict";
 let hex = "#bada55";
-convertHex(hex);
+hexToRGB(hex);
 
-function convertHex(hex) {
+function hexToRGB(hex) {
     let r = parseInt(hex.substring(1, 3), 16);
     let g = parseInt(hex.substring(3, 5), 16);
     let b = parseInt(hex.substring(5, 7), 16);
@@ -13,3 +13,21 @@ function convertHex(hex) {
         b
     });
 }
+
+
+const rgb = {
+    r: 1,
+    g: 2,
+    b: 3
+};
+
+rgbToHex(rgb);
+
+function rgbToHex(rgb) {
+    hex = "#" + ((1 << 24) + (rgb.r << 16) + (rgb.g << 8) + rgb.b).toString(16).slice(1);
+    console.log(hex);
+}
+
+/* 
+#0c0211
+*/
